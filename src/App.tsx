12c69582545e5fb5b26dc8dd00e08ff0267ea4a4
Stavros Symeonidis <stavrossymeonidis95@gmail.com>
2025-07-16 10:37:36 +0100

    First ever commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-6xl font-bold bg-gradient-to-r from-blue-600 via-purple-600 to-indigo-600 bg-clip-text text-transparent">
+        Welcome to the Future
+      </h1>
+    </div>
+  )
+}
+
+export default App
