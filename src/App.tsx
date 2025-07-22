653cf56369556e16dbf9bccf3fc090001c6f24b6
Stavros Symeonidis <stavrossymeonidis95@gmail.com>
2025-07-22 13:59:17 +0100

    Changed the default mode into dark and the background is now #000000

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { ThemeProvider } from "@/components/theme-provider";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
+    </ThemeProvider>
   );
 }
 
