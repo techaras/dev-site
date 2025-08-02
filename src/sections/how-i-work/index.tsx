df3790bb00cbe98776a7974ffc9c4bf34eab100c
Stavros Symeonidis <stavrossymeonidis95@gmail.com>
2025-08-02 16:35:09 +0100

    refactor: restructure HowIWork bento grid layout
    - Change from 3x2 grid to custom layout with stacked elements
    - Stack ResponseTime and Contact in left column of bottom row
    - Expand Services to span 2 columns on right side
    - Reduce bento square min-height from 300px to 200px
    - Remove flexbox centering to allow more flexible content layouts

--- src/sections/how-i-work/index.tsx
+++ src/sections/how-i-work/index.tsx
@@ -12,9 +12,9 @@ export function HowIWork() {
       className="py-20 px-6 md:px-12 lg:px-16"
     >
       <div className="w-full max-w-7xl mx-auto">
-
         {/* Bento Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-fr">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 auto-rows-fr">
+          {/* Top Row - 3 equal columns */}
           {/* Methodology */}
           <div className="bento-square">
             <Methodology />
@@ -30,18 +30,21 @@ export function HowIWork() {
             <Capabilities />
           </div>
 
-          {/* Response Time */}
-          <div className="bento-square">
-            <ResponseTime />
+          {/* Bottom Left - Stacked Response Time and Contact */}
+          <div className="flex flex-col gap-6">
+            {/* Response Time */}
+            <div className="bento-square">
+              <ResponseTime />
+            </div>
+
+            {/* Contact */}
+            <div className="bento-square">
+              <Contact />
+            </div>
           </div>
 
-          {/* Contact */}
-          <div className="bento-square">
-            <Contact />
-          </div>
-
-          {/* Services */}
-          <div className="bento-square">
+          {/* Services - Spans 2 columns */}
+          <div className="bento-square md:col-span-2">
             <Services />
           </div>
         </div>
