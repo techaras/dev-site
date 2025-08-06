dee20d2280e7fb71ce78e94730cdf615f2f9ec10
Stavros Symeonidis <stavrossymeonidis95@gmail.com>
2025-08-06 15:58:11 +0100

    Added the following 4 icons: Meta, Google Ads, LLMs and Supabase

---
+++ src/components/icons/skills/supabase-icon.tsx
@@ -0,0 +1,55 @@
+export function SupabaseIcon({ className }: { className?: string }) {
+    return (
+      <svg 
+        className={className}
+        width="54" 
+        height="56" 
+        viewBox="0 0 54 56" 
+        fill="none" 
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g clipPath="url(#clip0_212_219)">
+          <path 
+            d="M31.5615 54.6539C30.1449 56.4385 27.2725 55.4607 27.2384 53.1821L26.7393 19.854H49.1417C53.1994 19.854 55.4624 24.5422 52.9393 27.7211L31.5615 54.6539Z" 
+            fill="url(#paint0_linear_212_219)"
+          />
+          <path 
+            d="M31.5615 54.6539C30.1449 56.4385 27.2725 55.4607 27.2384 53.1821L26.7393 19.854H49.1417C53.1994 19.854 55.4624 24.5422 52.9393 27.7211L31.5615 54.6539Z" 
+            fill="url(#paint1_linear_212_219)" 
+            fillOpacity="0.2"
+          />
+          <path 
+            d="M22.4511 1.02668C23.8677 -0.758089 26.7401 0.219833 26.7743 2.49852L26.993 35.8265H4.87095C0.813144 35.8265 -1.44997 31.1384 1.0733 27.9594L22.4511 1.02668Z" 
+            fill="#3ECF8E"
+          />
+        </g>
+        <defs>
+          <linearGradient 
+            id="paint0_linear_212_219" 
+            x1="26.7393" 
+            y1="27.2437" 
+            x2="46.6517" 
+            y2="35.5922" 
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#249361"/>
+            <stop offset="1" stopColor="#3ECF8E"/>
+          </linearGradient>
+          <linearGradient 
+            id="paint1_linear_212_219" 
+            x1="17.912" 
+            y1="15.1536" 
+            x2="26.9969" 
+            y2="32.2498" 
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop/>
+            <stop offset="1" stopOpacity="0"/>
+          </linearGradient>
+          <clipPath id="clip0_212_219">
+            <rect width="54" height="56" fill="white"/>
+          </clipPath>
+        </defs>
+      </svg>
+    )
+  }
