931517d8365b6403583b7d3ac2c655ea533a9413
Stavros Symeonidis <stavrossymeonidis95@gmail.com>
2025-08-18 11:28:45 +0300

    fix: suppress exhaustive-deps warning in magnetic component
    Motion values x and y are stable references that don't need to be
    included in useEffect dependencies

--- src/components/ui/magnetic.tsx
+++ src/components/ui/magnetic.tsx
@@ -61,6 +61,7 @@ export function Magnetic({
     return () => {
       document.removeEventListener('mousemove', calculateDistance);
     };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [ref, isHovered, intensity, range]);
 
   useEffect(() => {
