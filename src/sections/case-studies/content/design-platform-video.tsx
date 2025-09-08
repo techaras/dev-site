807d3d57a7d5f81046188b95f1c4bf846a1b5944
Stavros Symeonidis <stavrossymeonidis95@gmail.com>
2025-09-08 15:50:50 +0100

    Changed the video scale to 112 from 110 in design-platform-video

--- src/sections/case-studies/content/design-platform-video.tsx
+++ src/sections/case-studies/content/design-platform-video.tsx
@@ -5,7 +5,7 @@ export function DesignPlatformVideo() {
     <div className="w-full h-full overflow-hidden">
       <CustomVideoPlayer
         publicId="design-video_fvioff"
-        className="w-full h-full [&>video]:object-cover [&>video]:object-top [&>video]:w-full [&>video]:h-full [&>video]:scale-110"
+        className="w-full h-full [&>video]:object-cover [&>video]:object-top [&>video]:w-full [&>video]:h-full [&>video]:scale-112"
         muted={true}
         onPlay={() => console.log('Design platform video started playing')}
         onPause={() => console.log('Design platform video paused')}
