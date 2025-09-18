f26650a56d022df511a3cf32dc6e8cfcc0fdcb6e
Stavros Symeonidis <stavrossymeonidis95@gmail.com>
2025-09-18 15:16:50 +0100

    Added mobile-footer to the mobile app

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { ProgressiveBlur } from "@/components/ui/progressive-blur";
 import { SkillsMobile } from "./sections/skills-mobile";
 import { AboutMeMobile } from "./sections/about-me-mobile";
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { MobileFooter } from "./sections/footer-mobile";
 
 function DesktopApp() {
   return (
@@ -53,6 +54,10 @@ function MobileApp() {
 
       {/* Mobile About Me Section */}
       <AboutMeMobile />
+
+      {/* Mobile Footer Section */}
+      <MobileFooter />
+
     </div>
   );
 }
