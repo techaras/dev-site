792323a845a1251b50b82a789b1a7ab4bad362df
Stavros Symeonidis <stavrossymeonidis95@gmail.com>
2025-11-27 11:28:45 +0000

    Updated video functionality

--- src/sections/case-studies/content/advertising-platform-video.tsx
+++ src/sections/case-studies/content/advertising-platform-video.tsx
@@ -3,7 +3,7 @@ import { CustomVideoPlayer } from "@/components/ui/custom-video-player";
 export function AdvertisingPlatformVideo() {
   return (
     <CustomVideoPlayer
-      publicId="ad-video_djpqvs"
+      publicId="ad-video_zxdsay"
       className="w-full h-full overflow-hidden"
       muted={true}
       onPlay={() => console.log('Advertising platform video started playing')}
--- src/sections/case-studies/content/design-platform-video.tsx
+++ src/sections/case-studies/content/design-platform-video.tsx
@@ -4,7 +4,7 @@ export function DesignPlatformVideo() {
   return (
     <div className="w-full h-full overflow-hidden">
       <CustomVideoPlayer
-        publicId="design-video_fvioff"
+        publicId="design-video_roslvu"
         className="w-full h-full [&>video]:object-cover [&>video]:object-top [&>video]:w-full [&>video]:h-full [&>video]:scale-112"
         muted={true}
         onPlay={() => console.log('Design platform video started playing')}
